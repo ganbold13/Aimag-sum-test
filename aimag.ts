2da1520e61f92651fbfc354722bf0ffb44ee3889
Ganbold <g.h.ganbold13@gmail.com>
2022-03-14 13:41:53 +0800

    add datat

---
+++ aimag.ts
@@ -0,0 +1,56 @@
+export const data = [
+  {
+    id: 1,
+    name: 'Arkhangai',
+    sum: [
+      {
+        name: 'ArkhangaiSum1',
+        bag: ['Bag11', 'Bag21', 'Bag31'],
+      },
+      {
+        name: 'ArkhangaiSum2',
+        bag: ['Bag12', 'Bag22', 'Bag32'],
+      },
+      {
+        name: 'ArkhangaiSum3',
+        bag: ['Bag13', 'Bag23', 'Bag33'],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Baynkhongor',
+    sum: [
+      {
+        name: 'BSum1',
+        bag: ['b1Bag1', 'b1Bag2', 'b1Bag3'],
+      },
+      {
+        name: 'BSum2',
+        bag: ['b2Bag1', 'Bag2', 'b2Bag3'],
+      },
+      {
+        name: 'BSum3',
+        bag: ['B3Bag1', 'b3Bag2', 'b3Bag3'],
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Bayn-Ulgii',
+    sum: [
+      {
+        name: 'BuSum1',
+        bag: ['Bu1Bag1', 'Bu1Bag2', 'Bu1Bag3'],
+      },
+      {
+        name: 'BuSum2',
+        bag: ['Bu2Bag1', 'Bu2Bag2', 'Bu2Bag3'],
+      },
+      {
+        name: 'BuSum3',
+        bag: ['Bu3Bag1', 'Bu3Bag2', 'Bu3Bag3'],
+      },
+    ],
+  },
+]
